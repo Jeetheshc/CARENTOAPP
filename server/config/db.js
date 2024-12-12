7e222608830015c3c5933b446e6ccc2bc6d0edc7
JEETHESH <jeetheshc@gmail.com>
2024-12-12 22:42:40 +0530

    mm1

--- server/config/db.js
+++ server/config/db.js
@@ -2,9 +2,16 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      
+    await mongoose.connect('mongodb+srv://jeetheshc:TUSRozswTTbC3VhP@cluster0.zsf5t.mongodb.net/carento_1', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
+
+
+
+    //   await mongoose.connect(process.env.MONGO_URI, {
+
+    //   });
     console.log('MongoDB connected');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
